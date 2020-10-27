1a1669668a449efc5ef9ef6d181b5733f9f785ea
Dylan Hensel <dylan@hensel.com>
2020-10-27 11:54:48 -0700

    (adds) dotenv reference as the db model, also lays out schemas, and allows connection to postgreSQL DB
    Co-authored-by: Guizzettic <guizzettic@gmail.com>

--- server/model/model.js
+++ server/model/model.js
@@ -1,1 +1,24 @@
 const Pool = require('pg');
+require('dotenv').config();
+
+const PG_URI = process.env.DB_URI;                                                                 
+
+const pool = new Pool({
+    connectionString: PG_URI,
+  });
+  
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query --> ', text);
+    return pool.query(text, params, callback);
+  },
+};
+
+/*
+ DB schemas below for reference 
+
+CREATE TABLE questions (_id SERIAL PRIMARY KEY, question VARCHAR(255) NOT NULL);
+
+CREATE TABLE questionaire (_id SERIAL PRIMARY KEY, question1 INT FOREIGN KEY(questions._id) NOT NULL, question2 INT FOREIGN KEY(questions._id), question3 INT FOREIGN KEY(questions._id), question4 INT FOREIGN KEY(questions._id), question5 INT FOREIGN KEY(questions._id), created_at TIMESTAMPTZ NOT NULL DEFAULT NOW());
+
+*/
