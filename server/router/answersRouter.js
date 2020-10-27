3a4c372936c6b35a152143e2855c1522b86b290c
Dylan Hensel <dylan@hensel.com>
2020-10-27 16:57:18 -0700

    (updates) answers router
    Co-authored-by: Guizzettic <guizzettic@gmail.com>

--- server/router/answersRouter.js
+++ server/router/answersRouter.js
@@ -2,5 +2,10 @@ const express = require('express');
 const answersRouter = express.Router();
 
 // get request pulling down user feed
+answersRouter.get()
+
+// posting feedback response to database
+answersRouter.post()
+
 
 module.exports = answersRouter;
