611de2d85b29683aea5a36242ddd618e584d3aae
Case Simmons <casesimmons@pm.me>
2020-10-28 11:32:43 -0700

    (update) Home page
    - Implement styling for entire page
    - Updated button to ToucableOpacity

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
-export default function Home({navigation}) {
-
+export default function Home({ navigation }) {
   const pressHandler = () => {
-    navigation.push('CreateFeedback')
-  }
+    navigation.push('CreateFeedback');
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Button 
-      title = 'Next'
-      onPress = {pressHandler}/>
+      <Text style={styles.logoStyle}>Critique</Text>
+      <Text style={styles.subHeader}>A Feedback Request App</Text>
+      <TouchableOpacity>
+        <Text style={styles.linkStyle} onPress={pressHandler}>
+          Login
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 24,
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoStyle: {
+    fontSize: 35,
+    fontFamily: 'Times New Roman',
+    color: 'white',
+    paddingBottom: 20,
+  },
+  subHeader: {
+    fontSize: 20,
+    fontFamily: 'Times New Roman',
+    color: 'lightgrey',
+    paddingBottom: 50,
+  },
+  linkStyle: {
+    fontSize: 18,
+    color: 'orange',
+    paddingBottom: 50,
   },
 });
