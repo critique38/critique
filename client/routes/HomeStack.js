c9063623622967a2f87f8788430ec1a8039698f8
Case Simmons <casesimmons@pm.me>
2020-10-28 11:44:20 -0700

    (update) Added Login screen to navigation stack

--- client/routes/HomeStack.js
+++ client/routes/HomeStack.js
@@ -1,37 +1,41 @@
 import { createStackNavigator } from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation'
+import { createAppContainer } from 'react-navigation';
 import React from 'react';
-import Home from '../components/Home'
+import Login from '../components/Login';
+import Home from '../components/Home';
 import SendFeedback from '../components/SendFeedback';
 import CreateFeedback from '../components/CreateFeedback';
-import Header from '../shared/Header'
+import Header from '../shared/Header';
 
 const screens = {
+  Login: {
+    screen: Login,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: () => <Header navigation={navigation} />,
+      };
+    },
+  },
   Home: {
     screen: Home,
-    navigationOptions : ({navigation}) => {
-      return {
-        headerTitle: () => <Header navigation = {navigation} />,
-      }
-    }
-  }, 
+  },
   CreateFeedback: {
     screen: CreateFeedback,
-  }, 
-  SendFeedback:{
-    screen:SendFeedback,
-  }
+  },
+  SendFeedback: {
+    screen: SendFeedback,
+  },
 };
 
 // home stack navigator screens
 const HomeStack = createStackNavigator(screens, {
-  defaultNavigationOptions:{
-    headerBackTitle:'Back',
-    title: 'Critique', 
-    headerStyle : {
-      backgroundColor: '#eee'
+  defaultNavigationOptions: {
+    headerBackTitle: 'Back',
+    title: 'Critique',
+    headerStyle: {
+      backgroundColor: '#eee',
     },
-  }
+  },
 });
 
 export default HomeStack;
