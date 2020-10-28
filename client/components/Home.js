afd65ebb95e8cf80ffaa521592bd785526969ec7
Case Simmons <casesimmons@pm.me>
2020-10-28 11:52:48 -0700

    (update) Home screen to be feed of outstanding Feedback
    - Create Feeback Touchable Opacity

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+// TODO import condensed Outstanding Questionaires
 
 export default function Home({ navigation }) {
   const pressHandler = () => {
     navigation.push('CreateFeedback');
   };
 
+  // Dummy data for username
+  const userName = <Text style={styles.userNameStyle}>Chris</Text>;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.logoStyle}>Critique</Text>
-      <Text style={styles.subHeader}>A Feedback Request App</Text>
-      <TouchableOpacity>
-        <Text style={styles.linkStyle} onPress={pressHandler}>
-          Login
-        </Text>
-      </TouchableOpacity>
+      <Text style={styles.logoStyle}>Critque</Text>
+      <View
+        style={{
+          borderBottomColor: 'grey',
+          borderBottomWidth: 1,
+        }}
+      />
+      <Text style={styles.requestStyle}>
+        {userName} requests your feedback for the following questions:
+      </Text>
+      {/* <Questionaire /> */}
+      <View style={styles.linkContainer}>
+        <TouchableOpacity>
+          <Text style={styles.linkStyle} onPress={pressHandler}>
+            Create Feedback
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -23,24 +38,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
   logoStyle: {
     fontSize: 35,
     fontFamily: 'Times New Roman',
     color: 'white',
-    paddingBottom: 20,
+    paddingTop: 60,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
   },
-  subHeader: {
-    fontSize: 20,
-    fontFamily: 'Times New Roman',
-    color: 'lightgrey',
-    paddingBottom: 50,
+  userNameStyle: {
+    fontSize: 16,
+    color: 'orange',
+  },
+  requestStyle: {
+    fontSize: 16,
+    color: 'white',
+    paddingTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  linkContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 30,
   },
   linkStyle: {
-    fontSize: 18,
-    color: 'orange',
-    paddingBottom: 50,
+    fontSize: 20,
+    color: '#1e90ff',
   },
 });
