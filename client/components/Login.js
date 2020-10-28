c9b990de6409c47d40dbc8a9439b11b7ea91189d
Case Simmons <casesimmons@pm.me>
2020-10-28 11:42:22 -0700

    (add) Login page to handle OAuth
    - Styled
    - Navigation leads to Home

---
+++ client/components/Login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+// OAuth implemented on this page
+
+export default function Login({ navigation }) {
+  const pressHandler = () => {
+    navigation.push('Home');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logoStyle}>Critique</Text>
+      <Text style={styles.subHeader}>A Feedback Request App</Text>
+      <TouchableOpacity>
+        <Text style={styles.linkStyle} onPress={pressHandler}>
+          Login
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoStyle: {
+    fontSize: 35,
+    fontFamily: 'Times New Roman',
+    color: 'white',
+    paddingBottom: 20,
+  },
+  subHeader: {
+    fontSize: 20,
+    fontFamily: 'Times New Roman',
+    color: 'lightgrey',
+    paddingBottom: 50,
+  },
+  linkStyle: {
+    fontSize: 18,
+    color: 'orange',
+    paddingBottom: 50,
+  },
+});
