b74a6d43456e8af49cf74a52a22e2247fecf599b
Chris Guizzetti <guizzettic@gmail.com>
2020-10-28 16:49:07 -0600

    (modify) modifying passport file
    Co-authored-by: DylanRH <dylan@hensel.com>
    Co-authored-by: casesimmons <casesimmons@protonmail.com>

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const env = require('./.env');
+const env = require('.././.env');
 const db = require('../server/model/model');
 require('dotenv').config();
 
