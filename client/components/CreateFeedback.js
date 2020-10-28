cc4f60c9dbb71efb6ba2a2d244094f7404dda43b
Joonyoung <impervious7@gmail.com>
2020-10-27 22:48:52 -0700

    (add) client folder with expo config

---
+++ client/components/CreateFeedback.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default function AnswerView() {
+
+    return (
+        <View>
+            
+        </View>
+    )
+
+}
+
+
+const styles = StyleSheet.create({
+
+})
