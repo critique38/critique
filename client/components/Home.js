48af669d3dd0f41e396006aa782ff606affece8f
Case Simmons <casesimmons@pm.me>
2020-10-28 17:10:41 -0700

    (update) Home Feed with FlatList to create each outstanding question
    - Dummy data in shape desired
    - Additional styling to focus on Request Feedback TouchableOpacity

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,31 +1,73 @@
-
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function Home({ navigation }) {
   const pressHandler = () => {
     navigation.push('CreateFeedback');
   };
 
-  // Dummy data for username
-  const userName = <Text style={styles.userNameStyle}>Chris</Text>;
-
-  // Hook for questionaire data:
-  const [questionaireData, setQuestionaireData] = useState({});
-
-  // Fetch outstanding questionaires feed onload
-  useEffect(() => {
-    fetch('/api/feed')
-      .then((res) => res.json())
-      .then((res) => setQuestionaireData(res))
-      .catch((err) =>
-        console.log('Failed to load outstanding questionaires from db:', err)
-      );
-  }, []);
-
   const touchHandler = () => {
     navigation.navigate('CreateAnswer');
   };
+
+  // dummy data
+  const questions = [
+    {
+      question_id: 2,
+      asker_id: 'Chris G.',
+      receiver_id: 3,
+      question: 'How can we flip this .08 as fast as possible?',
+    },
+    {
+      question_id: 3,
+      asker_id: 'Vincent',
+      receiver_id: 3,
+      question: `Could you rest tomorrow, take a break once we're done?`,
+    },
+    {
+      question_id: 4,
+      asker_id: 'Dylan',
+      receiver_id: 3,
+      question: "Why can't you exercise while programming, I do?",
+    },
+    {
+      question_id: 5,
+      asker_id: 'Joon',
+      receiver_id: 3,
+      question: 'What harsh truths do you prefer to ignore?',
+    },
+    {
+      question_id: 6,
+      asker_id: 'Vincent',
+      receiver_id: 3,
+      question: 'Will you be available at 11:30pm to work?',
+    },
+    {
+      question_id: 7,
+      asker_id: 'Joon',
+      receiver_id: 3,
+      question: 'Is free will real or just an illusion?',
+    },
+  ];
+
+  // Hook for questionaire data:
+  // const [questionaireData, setQuestionaireData] = useState({});
+
+  // Fetch outstanding questions fromd db, onload
+  // useEffect(() => {
+  //   fetch('/api/feed')
+  //     .then((res) => res.json())
+  //     .then((res) => setQuestionaireData(res))
+  //     .catch((err) =>
+  //       console.log('Failed to load outstanding questionaires from db:', err)
+  //     );
+  // }, []);
 
   return (
     <View style={styles.container}>
@@ -36,43 +78,76 @@ export default function Home({ navigation }) {
           borderBottomWidth: 1,
         }}
       />
-      <Text style={styles.requestStyle}>
+      <Text style={styles.outstandingStyle}>Outstanding Feedback:</Text>
+      <View style={styles.container}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(question) => question.q}
+          data={questions}
+          renderItem={({ item }) => {
+            // element === { item: { q: 'How could I improve on presenting myself?'}, index: 0}
+            return (
+              <View style={styles.questions}>
+                <TouchableOpacity>
+                  <Text style={styles.userNameStyle} onPress={touchHandler}>
+                    {item.asker_id}
+                  </Text>
+                  <Text style={styles.requestStyle} onPress={touchHandler}>
+                    "{item.question}"
+                  </Text>
+                </TouchableOpacity>
+                <View
+                  style={{
+                    paddingTop: 20,
+                    borderBottomColor: 'grey',
+                    borderBottomWidth: 1,
+                  }}
+                />
+              </View>
+            );
+          }}
+        />
+      </View>
+      {/* <Text style={styles.requestStyle}>
         {userName} requests your feedback for the following questions:
       </Text>
-     <TouchableOpacity>
-          <Text onPress={touchHandler}>Sample Question from Joon Kim</Text>
-        </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.requestStyle} onPress={touchHandler}>
+          Sample Question from Joon Kim
+        </Text>
+      </TouchableOpacity> */}
       {/* <Questionaire /> */}
       <View style={styles.linkContainer}>
         <TouchableOpacity>
           <Text style={styles.linkStyle} onPress={pressHandler}>
-            Create Feedback
+            Request Feedback
           </Text>
         </TouchableOpacity>
       </View>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    padding: 10,
-  },
   logoStyle: {
     fontSize: 35,
     fontFamily: 'Times New Roman',
     color: 'white',
-    paddingTop: 60,
+    paddingTop: 30,
     paddingLeft: 10,
     paddingRight: 10,
     paddingBottom: 10,
   },
+  container: {
+    flex: 10,
+    backgroundColor: 'black',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
   userNameStyle: {
     fontSize: 16,
     color: 'orange',
+    paddingTop: 15,
   },
   requestStyle: {
     fontSize: 16,
@@ -81,11 +156,20 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
   },
+  outstandingStyle: {
+    fontSize: 18,
+    color: '#1e90ff',
+    paddingTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 15,
+  },
   linkContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
-    paddingTop: 30,
+    paddingTop: 20,
+    paddingBottom: 20,
   },
   linkStyle: {
     fontSize: 20,
