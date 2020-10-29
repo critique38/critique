bcb77964398486a36de5d8967b30562b7ada7ee6
Case Simmons <casesimmons@pm.me>
2020-10-29 12:00:09 -0700

    (update) CreateFeedback, Home, SendFeedback
    Co-authored-by: vnguyencode <vnguyencode@gmail.com>

--- client/shared/Header.js
+++ client/shared/Header.js
@@ -1,45 +1,44 @@
-import React from 'react'; 
-import {StyleSheet, Text, View } from 'react-native'; 
-import { MaterialIcons } from '@expo/vector-icons'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function Header ({navigation}){
-  
+export default function Header({ navigation }) {
   const openMenu = () => {
-    navigation.openDrawer()
-  }
+    navigation.openDrawer();
+  };
 
   return (
-    <View style = {styles.header}>
+    <View style={styles.header}>
       <MaterialIcons
-      name = 'menu'
-      size = {28}
-      onPress = {openMenu}
-      style = {styles.icon}
+        name="menu"
+        size={28}
+        onPress={openMenu}
+        style={styles.icon}
       />
-    <View>
-      <Text style = {styles.headerText}>
-        Critique
-      </Text>
+      {/* <View>
+        <Text style={styles.headerText}>Critique</Text>
+      </View> */}
     </View>
-    </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   header: {
-    width: '100%', 
-    height: '100%', 
-    flexDirection: 'row',
-    alignItems: 'center', 
-    justifyContent: 'center',
-  }, 
-  headerText: {
-    fontWeight:'bold', 
-    fontSize: 20, 
-    color: '#333'
+    // width: '100%',
+    // height: '100%',
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    backgroundColor: 'black',
   },
-  icon:{
-    position:'absolute',
-    left:-50
-  }
-})
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'orange',
+  },
+  icon: {
+    position: 'absolute',
+    color: 'orange',
+    left: -50,
+  },
+});
